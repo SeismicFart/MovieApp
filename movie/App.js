506e8c9a53bd61b5c872cd57e21be08cc5c375d2
aa6622 <kufupa@users.noreply.github.com>
2023-09-06 23:55:58 +0100

    display api key

--- movie/App.js
+++ movie/App.js
@@ -1,8 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import 'dotenv/config'
 
 export default function App() {
   return (
+    <View>
+      <Text>API Key: {process.env.API_KEY}</Text>
+    </View>
     <AppNavigation />
   );
 }
